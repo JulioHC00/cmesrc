dd0fc42df197299cc0ead1aff885bbb3dfef9b86
Julio Hernandez Camero <julhcam@pm.me>
2023-01-19 11:17:53 +0000

    Update directory path definitions

--- src/cmesrc/config.py
+++ src/cmesrc/config.py
@@ -9,7 +9,11 @@ DATA_DIR = Path(ROOT.joinpath(Path("data/")))
 RAW_DATA_DIR = Path(DATA_DIR.joinpath(Path("./raw/")))
 INTERIM_DATA_DIR = Path(DATA_DIR.joinpath(Path("./interim/")))
 PROCESSED_DATA_DIR = Path(DATA_DIR.joinpath(Path("./processed/")))
+LASCO_DATA_DIR = Path(RAW_DATA_DIR.joinpath(Path("./lasco/")))
+MVTS_DATA_DIR = Path(RAW_DATA_DIR.joinpath(Path("./mvts/")))
+SWAN_DATA_DIR = Path(MVTS_DATA_DIR.joinpath(Path("./SWAN/")))
+PARTITIONS_DATA_DIR = Path(MVTS_DATA_DIR.joinpath(Path("./partitions/")))
 
 # Raw data files
 
-RAW_LASCO_CME_CATALOGUE = Path(RAW_DATA_DIR.joinpath("./univ_all.txt"))
+RAW_LASCO_CME_CATALOGUE = Path(LASCO_DATA_DIR.joinpath("./univ_all.txt"))
--- src/cmesrc/config_tests.py
+++ src/cmesrc/config_tests.py
@@ -14,3 +14,16 @@ def test_processed_data_dir():
 
 def test_existence_raw_cme_catalogue():
     assert config.RAW_LASCO_CME_CATALOGUE.exists()
+
+def test_existence_LASCO_dir():
+    assert config.LASCO_DATA_DIR.exists()
+
+def test_existence_MVTS_dir():
+    assert config.MVTS_DATA_DIR.exists()
+
+def test_existence_SWAN_dir():
+    assert config.SWAN_DATA_DIR.exists()
+
+def test_existence_partitions_dir():
+    assert config.PARTITIONS_DATA_DIR.exists()
+
