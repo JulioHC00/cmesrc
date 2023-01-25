2bdbd34801d067361f4860e6110a51621a3d71b6
Julio Hernandez Camero <julhcam@pm.me>
2023-01-25 14:21:09 +0000

    Change to using os functions

--- src/cmesrc/config.py
+++ src/cmesrc/config.py
@@ -3,23 +3,23 @@ from os.path import dirname, abspath, join
 
 # Paths to directories
 
-ROOT = Path(dirname(abspath("requirements.txt")))
+ROOT = Path(dirname(abspath("../requirements.txt")))
 
-DATA_DIR = ROOT.joinpath("data/")
-RAW_DATA_DIR = DATA_DIR.joinpath("./raw/")
-INTERIM_DATA_DIR = DATA_DIR.joinpath("./interim/")
-PROCESSED_DATA_DIR = DATA_DIR.joinpath("./processed/")
-LASCO_DATA_DIR = RAW_DATA_DIR.joinpath("./lasco/")
-MVTS_DATA_DIR = RAW_DATA_DIR.joinpath("./mvts/")
-SWAN_DATA_DIR = MVTS_DATA_DIR.joinpath("./SWAN/")
-PARTITIONS_DATA_DIR = MVTS_DATA_DIR.joinpath("./partitions/")
+DATA_DIR = join(ROOT, "data/")
+RAW_DATA_DIR = join(DATA_DIR, "raw/")
+INTERIM_DATA_DIR = join(DATA_DIR, "interim/")
+PROCESSED_DATA_DIR = join(DATA_DIR, "processed/")
+LASCO_DATA_DIR = join(RAW_DATA_DIR, "lasco/")
+MVTS_DATA_DIR = join(RAW_DATA_DIR, "mvts/")
+SWAN_DATA_DIR = join(MVTS_DATA_DIR, "SWAN/")
+PARTITIONS_DATA_DIR = join(MVTS_DATA_DIR, "partitions/")
 
 # Raw data files
 
-RAW_LASCO_CME_CATALOGUE = Path(LASCO_DATA_DIR.joinpath("./univ_all.txt"))
+RAW_LASCO_CME_CATALOGUE = join(LASCO_DATA_DIR, "univ_all.txt")
 
 # Interim data files
 
-HARPS_LIFETIME_DATABSE = Path(INTERIM_DATA_DIR.joinpath("./harps_lifetime_database.csv"))
-LASCO_CME_DATABASE = Path(INTERIM_DATA_DIR.joinpath("./lasco_cme_database.csv"))
-TEMPORAL_MATCHING_HARPS_DATABASE = Path(INTERIM_DATA_DIR.joinpath("./temporal_matching_harps_database.csv"))
+HARPS_LIFETIME_DATABSE = join(INTERIM_DATA_DIR, "harps_lifetime_database.csv")
+LASCO_CME_DATABASE = join(INTERIM_DATA_DIR, "lasco_cme_database.csv")
+TEMPORAL_MATCHING_HARPS_DATABASE = join(INTERIM_DATA_DIR, "temporal_matching_harps_database.csv")
--- src/cmesrc/test_config.py
+++ src/cmesrc/test_config.py
@@ -1,29 +1,30 @@
 from src.cmesrc import config
+from os.path import exists
 
 def test_data_dir():
-    assert config.DATA_DIR.exists()
+    assert exists(config.DATA_DIR)
     
 def test_raw_data_dir():
-    assert config.RAW_DATA_DIR.exists()
+    assert exists(config.RAW_DATA_DIR)
     
 def test_interim_data_dir():
-    assert config.INTERIM_DATA_DIR.exists()
+    assert exists(config.INTERIM_DATA_DIR)
 
 def test_processed_data_dir():
-    assert config.PROCESSED_DATA_DIR.exists()
+    assert exists(config.PROCESSED_DATA_DIR)
 
 def test_existence_raw_cme_catalogue():
-    assert config.RAW_LASCO_CME_CATALOGUE.exists()
+    assert exists(config.RAW_LASCO_CME_CATALOGUE)
 
 def test_existence_LASCO_dir():
-    assert config.LASCO_DATA_DIR.exists()
+    assert exists(config.LASCO_DATA_DIR)
 
 def test_existence_MVTS_dir():
-    assert config.MVTS_DATA_DIR.exists()
+    assert exists(config.MVTS_DATA_DIR)
 
 def test_existence_SWAN_dir():
-    assert config.SWAN_DATA_DIR.exists()
+    assert exists(config.SWAN_DATA_DIR)
 
 def test_existence_partitions_dir():
-    assert config.PARTITIONS_DATA_DIR.exists()
+    assert exists(config.PARTITIONS_DATA_DIR)
 
