1d7c4b03c5a9d7fb1cee3983609457369206732d
Julio Hernandez Camero <julhcam@pm.me>
2023-01-25 14:13:20 +0000

    Remove further unnecessary paths

--- src/cmesrc/config.py
+++ src/cmesrc/config.py
@@ -1,18 +1,18 @@
 from pathlib import Path
-from os.path import dirname, abspath
+from os.path import dirname, abspath, join
 
 # Paths to directories
 
 ROOT = Path(dirname(abspath("requirements.txt")))
 
-DATA_DIR = ROOT.joinpath(Path("data/"))
+DATA_DIR = ROOT.joinpath("data/")
 RAW_DATA_DIR = DATA_DIR.joinpath("./raw/")
-INTERIM_DATA_DIR = DATA_DIR.joinpath(Path("./interim/"))
-PROCESSED_DATA_DIR = DATA_DIR.joinpath(Path("./processed/"))
-LASCO_DATA_DIR = RAW_DATA_DIR.joinpath(Path("./lasco/"))
-MVTS_DATA_DIR = RAW_DATA_DIR.joinpath(Path("./mvts/"))
-SWAN_DATA_DIR = MVTS_DATA_DIR.joinpath(Path("./SWAN/"))
-PARTITIONS_DATA_DIR = MVTS_DATA_DIR.joinpath(Path("./partitions/"))
+INTERIM_DATA_DIR = DATA_DIR.joinpath("./interim/")
+PROCESSED_DATA_DIR = DATA_DIR.joinpath("./processed/")
+LASCO_DATA_DIR = RAW_DATA_DIR.joinpath("./lasco/")
+MVTS_DATA_DIR = RAW_DATA_DIR.joinpath("./mvts/")
+SWAN_DATA_DIR = MVTS_DATA_DIR.joinpath("./SWAN/")
+PARTITIONS_DATA_DIR = MVTS_DATA_DIR.joinpath("./partitions/")
 
 # Raw data files
 
