1e6351753d66b2aecf8347222aca39ba77e0816f
Julio Hernandez Camero <julhcam@pm.me>
2023-01-27 11:02:51 +0000

    Make output of temporal matching contain all rows of LASCO catalogue instead of just date, harp matches and number of matches

--- src/scripts/spatiotemporal_matching/temporal_matching.py
+++ src/scripts/spatiotemporal_matching/temporal_matching.py
@@ -12,36 +12,42 @@ import pandas as pd
 lasco_cme_database = pd.read_csv(LASCO_CME_DATABASE)
 harps_lifetime_database = pd.read_csv(HARPS_LIFETIME_DATABSE)
 
-
 harps_lifetime_start_times = np.array([Time(harps_time) for harps_time in harps_lifetime_database["start"]])
 harps_lifetime_end_times = np.array([Time(harps_time) for harps_time in harps_lifetime_database["end"]])
-cme_detection_times = np.array([Time(cme_time) for cme_time in lasco_cme_database["date"]])
+lasco_cme_database["date"] = np.array([Time(cme_time) for cme_time in lasco_cme_database["date"]]) # Parse dates
+cme_detection_times = lasco_cme_database["date"].to_numpy()
 
 FIRST_AVAILABLE_HARPS = min(harps_lifetime_start_times)
  
 CME_TIME_MASK = cme_detection_times > FIRST_AVAILABLE_HARPS
 CME_QUALITY_MASK = (lasco_cme_database["quality"] == 0) & (lasco_cme_database["three_points"] == 0) # CMEs withou poor or very poor flags and without few points warning.
+CME_FULL_MASK = CME_TIME_MASK & CME_QUALITY_MASK
 
-def findMatchingHarpsRegions(cme_detection_time):
+masked_lasco_cme_database = lasco_cme_database[CME_FULL_MASK]
+
+def findMatchingHarpsRegions(cme_detection_time: str) -> np.ndarray:
 
     matching_harps_mask = (harps_lifetime_start_times <= cme_detection_time) & (harps_lifetime_end_times >= cme_detection_time)
 
     matching_harps_list = harps_lifetime_database[matching_harps_mask]["harpsnum"].to_numpy()
 
-    cme_date_with_harps_list = {
-            "cme_date": str(cme_detection_time),
-            "matching_harps": matching_harps_list,
-            "n_matching_harps": len(matching_harps_list)
-            }
+    return matching_harps_list
 
-    return cme_date_with_harps_list
 
 def findAllMatchingRegions():
     print("Finding HARPS that match CMEs temporally")
     full_list_of_matches = []
-#    full_list_of_matches = [findMatchingHarpsRegions(cme_detection_time) for cme_detection_time in cme_detection_times[CME_MASK]]
-    for cme_detection_time in tqdm(cme_detection_times[CME_TIME_MASK & CME_QUALITY_MASK]):
-        full_list_of_matches.append(findMatchingHarpsRegions(cme_detection_time) )
+
+    for i, row in tqdm(masked_lasco_cme_database.iterrows(), total=masked_lasco_cme_database.shape[0]):
+        cme_detection_time = row["date"]
+        matching_harps_list = findMatchingHarpsRegions(cme_detection_time)
+
+        new_row = row.to_dict()
+
+        new_row["matching_harps"] = matching_harps_list
+        new_row["n_matching_harps"] = len(matching_harps_list)
+
+        full_list_of_matches.append(new_row)
 
     temporal_matching_harps_database = pd.DataFrame.from_records(full_list_of_matches)
     temporal_matching_harps_database.to_csv(TEMPORAL_MATCHING_HARPS_DATABASE, index=False)
