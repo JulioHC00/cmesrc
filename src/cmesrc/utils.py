2d46e7f17fe0bec629b890b0ea0e3443def66cc3
Julio Hernandez Camero <julhcam@pm.me>
2023-01-30 09:46:03 +0000

    Update paths + Add utility to parse pandas lists and find closest time for a HARPS

--- src/cmesrc/utils.py
+++ src/cmesrc/utils.py
@@ -1,4 +1,7 @@
 from astropy.time import Time
+from datetime import datetime, timedelta
+import astropy.units as u
+from bisect import bisect_left
 
 def parse_date(date_str):
     if type(date_str) == Time:
@@ -7,3 +10,12 @@ def parse_date(date_str):
         return Time(date_str)
     else:
         raise ValueError("Input date must be either a string or a astropy Time object")
+
+def parse_pandas_str_list(str_list: str) -> list:
+    no_brackets_str_list = str_list.replace("[", "").replace("]", "").split()
+    parsed_list = [int(item) for item in no_brackets_str_list]
+    return parsed_list
+
+def get_closest_harps_timestamp(harps_timestamps, cme_time) -> Time:
+    i = bisect_left(harps_timestamps, cme_time)
+    return min(harps_timestamps[max(0, i-1): i+2], key=lambda t: abs(cme_time - t))
