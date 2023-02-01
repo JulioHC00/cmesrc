a015c19cbaf07feec09da5ae6ad8aba30c14b79d
Julio Hernandez Camero <julhcam@pm.me>
2023-02-01 14:25:22 +0000

    Modify Harps classes to be derived from BoundingBox class

--- src/harps/harps.py
+++ src/harps/harps.py
@@ -1,121 +1,35 @@
-from astropy.time import Time
-from astropy.coordinates import SkyCoord
-import astropy.units as u
-from sunpy.coordinates import HeliographicStonyhurst, propagate_with_solar_surface
-import numpy as np
+from src.cmesrc.classes import BoundingBox, RotatedBoundingBox
 
-PI = np.pi
-DEG_TO_RAD = PI / 180
-
-class InvalidBoundingBox(Exception):
-    """
-    To be raised when a bounding box is not valid
-    """
-    def __init__(self, LON_MIN: float, LAT_MIN: float, LON_MAX: float, LAT_MAX: float):
-        # Set values
-        self.LON_MIN = LON_MIN
-        self.LAT_MIN = LAT_MIN
-        self.LON_MAX = LON_MAX
-        self.LAT_MAX = LAT_MAX
-
-        self.message = f"Bounding box [(lonmin, lonmax) (latmin, latmax)] [({self.LON_MIN},{self.LON_MAX}), ({self.LAT_MIN}, {self.LAT_MAX})] is not valid"
-        super().__init__(self.message)
-        
-
-class BaseHarps:
-    def __init__(self, T_REC, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX, HARPNUM=None):
-        self.T_REC = Time(T_REC, format="iso")
-        self.LON_MIN = float(LON_MIN)
-        self.LAT_MIN = float(LAT_MIN)
-        self.LON_MAX = float(LON_MAX)
-        self.LAT_MAX = float(LAT_MAX)
-        self.FRAME = HeliographicStonyhurst(obstime=self.T_REC)
+class RotatedHarps(RotatedBoundingBox):
+    def __init__(self, date, lon_min, lat_min, lon_max, lat_max, HARPNUM=None):
+        super().__init__(
+                date=date,
+                lon_min=lon_min,
+                lat_min=lat_min,
+                lon_max=lon_max,
+                lat_max=lat_max
+                )
         self.HARPNUM = HARPNUM
-        self.__ROTATED = None
 
-        self.__check_input_coordinates()
+class Harps(BoundingBox):
+    def __init__(self, date, lon_min, lat_min, lon_max, lat_max, HARPNUM=None):
+        super().__init__(
+                date=date,
+                lon_min=lon_min,
+                lat_min=lat_min,
+                lon_max=lon_max,
+                lat_max=lat_max
+                )
+        self.HARPNUM = HARPNUM
 
-        self.RAW_CENTER_POINT = self._calculateRawCentrePoint()
-        self.CARTESIAN_CENTER_POINT = self._calculateCartesianCoords()
-        self.POSITION_ANGLE = self._calculatePositionAngle()
-        self.DISTANCE_TO_SUN_CENTRE = self._calculateDistanceToSunCentre()
-
-    def __check_input_coordinates(self):
-        if (self.LON_MIN > self.LON_MAX) or (self.LAT_MIN > self.LAT_MAX):
-            raise InvalidBoundingBox(self.LON_MIN, self.LAT_MIN, self.LON_MAX, self.LAT_MAX)
-
-    def is_rotated(self):
-        return self.__ROTATED
-
-    def _set_rotation(self, rotation):
-        self.__ROTATED = rotation
-
-    def _calculatePositionAngle(self) -> np.float64:
-
-        position_angle = np.arctan2(self.CARTESIAN_CENTER_POINT[1], self.CARTESIAN_CENTER_POINT[0]) * 180 / np.pi
-
-        if 0 <= position_angle <= 90:
-            position_angle += 270
-        elif 90 < position_angle <= 180:
-            position_angle -= 90
-        elif position_angle < 0:
-            position_angle += 270
-
-        return position_angle
-
-    def _calculateCartesianCoords(self) -> np.ndarray:
-        return np.array([
-            np.cos(self.RAW_CENTER_POINT[1] * DEG_TO_RAD) * np.sin(self.RAW_CENTER_POINT[0] * DEG_TO_RAD),
-            np.sin(self.RAW_CENTER_POINT[1] * DEG_TO_RAD)
-            ])
-
-    def _calculateRawCentrePoint(self) -> np.ndarray:
-        return np.array([
-            np.average([self.LON_MIN, self.LON_MAX]),
-            np.average([self.LAT_MIN, self.LAT_MAX])
-            ])
-
-    def _calculateDistanceToSunCentre(self) -> np.float64:
-        return np.sqrt(self.CARTESIAN_CENTER_POINT[0] ** 2 + self.CARTESIAN_CENTER_POINT[1] ** 2)
-
-    def get_raw_bounding_box(self):
-        return [[self.LON_MIN, self.LAT_MIN], [self.LON_MAX, self.LAT_MAX]]
-
-    def get_skycoord_bounding_box(self):
-        return SkyCoord([
-            [self.LON_MIN * u.deg, self.LAT_MIN * u.deg],
-            [self.LON_MAX * u.deg, self.LAT_MAX * u.deg]
-            ],
-                        frame=self.FRAME
-                        )
-
-class RotatedHarps(BaseHarps):
-    def __init__(self, T_REC, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX, HARPNUM=None):
-        super().__init__(T_REC, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX, HARPNUM)
-        self._set_rotation(True)
-
-class Harps(BaseHarps):
-    def __init__(self, T_REC, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX, HARPNUM=None):
-        super().__init__(T_REC, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX, HARPNUM)
-        self._set_rotation(False)
-
-    def rotate_coords(self, date):
-        original_bbox = self.get_skycoord_bounding_box()
-        new_frame = HeliographicStonyhurst(obstime = date)
-
-        with propagate_with_solar_surface():
-            new_bbox = original_bbox.transform_to(new_frame)
-
-        new_coords = new_bbox.to_string()
-
-        new_lon_min, new_lat_min = (float(coord) for coord in new_coords[0].split())
-        new_lon_max, new_lat_max = (float(coord) for coord in new_coords[1].split())
+    def rotate_bbox(self, date, inplace:bool = False):
+        new_bbox = super().rotate_bbox(date=date, inplace=inplace)
 
         return RotatedHarps(
-                T_REC = date,
-                LON_MIN = new_lon_min,
-                LAT_MIN = new_lat_min,
-                LON_MAX = new_lon_max,
-                LAT_MAX = new_lat_max,
+                date = new_bbox.DATE,
+                lon_min = new_bbox.LOWER_LEFT.LON,
+                lat_min = new_bbox.LOWER_LEFT.LAT,
+                lon_max = new_bbox.UPPER_RIGHT.LON,
+                lat_max = new_bbox.UPPER_RIGHT.LAT,
                 HARPNUM = self.HARPNUM
                 )
--- src/harps/test_harps.py
+++ src/harps/test_harps.py
@@ -1,5 +1,7 @@
-from src.harps.harps import Harps, RotatedHarps, InvalidBoundingBox
+from src.harps.harps import Harps, RotatedHarps
+from src.cmesrc.exception_classes import InvalidBoundingBox
 from sunpy.coordinates import HeliographicStonyhurst, propagate_with_solar_surface
+from src.cmesrc.classes import BoundingBox, RotatedBoundingBox
 import numpy as np
 import pytest
 
@@ -11,17 +13,9 @@ def test_good_initialization_of_harps():
     LAT_MAX = 28
     harps = Harps(TIME, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX)
 
-    test_time = (harps.T_REC == TIME)
-    test_coords = np.array([
-        (harps.LON_MIN == LON_MIN),
-        (harps.LAT_MIN == LAT_MIN),
-        (harps.LON_MAX == LON_MAX),
-        (harps.LAT_MAX == LAT_MAX),
+    assert np.all([
+        np.all([[23,24], [26,28]] == harps.get_raw_bbox())
         ])
-
-    test = test_time & np.all(test_coords)
-
-    assert test
 
 def test_invalid_bounding_box():
     TIME = "2000-12-23 12:00:00"
@@ -43,17 +37,18 @@ LAT_MAX = 40
 def test_center_point_coords():
     harps = Harps(TIME, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX)
 
-    assert np.all(harps.RAW_CENTER_POINT == np.array([20, 30]))
+    assert np.all(harps.CENTRE_POINT.get_raw_coords() == np.array([20, 30]))
 
 @pytest.mark.depends(on=['test_center_point_coords'])
 def test_cartesian_center_coords():
     harps = Harps(TIME, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX)
+    centre_point = harps.CENTRE_POINT
     cartesian_centre = np.array([
-        np.cos(harps.RAW_CENTER_POINT[1] * np.pi / 180) * np.sin(harps.RAW_CENTER_POINT[0] * np.pi / 180),
-        np.sin(harps.RAW_CENTER_POINT[1] * np.pi / 180)
+        np.cos(centre_point.LAT * np.pi / 180) * np.sin(centre_point.LON * np.pi / 180),
+        np.sin(centre_point.LAT * np.pi / 180)
         ])
 
-    assert np.all(np.isclose(harps.CARTESIAN_CENTER_POINT, cartesian_centre))
+    assert np.all(np.isclose(harps.get_cartesian_centre_point(), cartesian_centre))
 
 @pytest.mark.depends(on=['test_cartesian_center_coords'])
 def test_position_angle():
@@ -62,46 +57,47 @@ def test_position_angle():
     LON_MAX = -20
     LAT_MAX = 40
     harps = Harps(TIME, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX)
+    centre_point = harps.get_cartesian_centre_point()
 
-    position_angle = np.arctan2(harps.CARTESIAN_CENTER_POINT[1], harps.CARTESIAN_CENTER_POINT[0]) * 180 / np.pi - 90
-    assert (np.isclose(position_angle, harps.POSITION_ANGLE)) and (position_angle < 90)
+    position_angle = np.arctan2(centre_point[1], centre_point[0]) * 180 / np.pi - 90
+    assert (np.isclose(position_angle, harps.get_position_angle())) and (position_angle < 90)
 
 @pytest.mark.depends(on=['test_cartesian_center_coords'])
 def test_distance_to_sun_center():
     harps = Harps(TIME, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX)
-    distance_to_sun_center = np.sqrt(harps.CARTESIAN_CENTER_POINT[0] ** 2 + harps.CARTESIAN_CENTER_POINT[1] ** 2)
+    centre_point = harps.get_cartesian_centre_point()
+    distance_to_sun_center = np.sqrt(centre_point[0] ** 2 + centre_point[1] ** 2)
 
-    assert np.isclose(distance_to_sun_center, harps.DISTANCE_TO_SUN_CENTRE)
+    assert np.isclose(distance_to_sun_center, harps.get_distance_to_sun_centre())
 
 def test_non_rotated():
-    harps = Harps(TIME, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX)
-    assert harps.is_rotated() == False
+    assert issubclass(Harps, BoundingBox)
 
 def test_rotated():
     harps = RotatedHarps(TIME, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX)
-    assert harps.is_rotated() == True
+    assert issubclass(RotatedHarps, RotatedBoundingBox)
 
 def test_rotation_returns_RotatedHarps():
     harps = Harps(TIME, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX)
 
-    rotated_harps = harps.rotate_coords("2000-12-23 12:00:01")
+    rotated_harps = harps.rotate_bbox("2000-12-23 12:00:01")
 
-    assert type(rotated_harps) == RotatedHarps
+    assert type(rotated_harps) is RotatedHarps
 
 def test_rotated_coordinates_values():
     harps = Harps(TIME, LON_MIN, LAT_MIN, LON_MAX, LAT_MAX)
 
     NEW_TIME = "2000-12-23 16:00:00"
 
-    rotated_harps = harps.rotate_coords(NEW_TIME)
+    rotated_harps = harps.rotate_bbox(NEW_TIME)
 
-    original_coords = harps.get_skycoord_bounding_box()
+    original_coords = harps.get_skycoord_bbox()
 
     new_frame = HeliographicStonyhurst(obstime=NEW_TIME)
 
     with propagate_with_solar_surface():
         new_coordinates = original_coords.transform_to(new_frame)
 
-    separation = new_coordinates.separation(rotated_harps.get_skycoord_bounding_box()).degree
+    separation = new_coordinates.separation(rotated_harps.get_skycoord_bbox()).degree
 
     assert np.all(separation < 1e-4)
