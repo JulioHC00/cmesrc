6ef9d0261050165ca52fd7d86450b1b902d43bef
Julio Hernandez Camero <julhcam@pm.me>
2023-02-27 15:07:02 +0000

    Update plots

--- notebooks/03-jhc-overview_plots.py
+++ notebooks/03-jhc-overview_plots.py
@@ -7,10 +7,11 @@ from tqdm import tqdm
 from astropy.time import Time
 from os.path import join
 from matplotlib.patches import Wedge, Rectangle
-from src.cmesrc.config import SWAN_DATA_DIR, ALL_MATCHING_HARPS_DATABASE_PICKLE, SCORED_HARPS_MATCHING_DIMMINGS_DATABASE_PICKLE, OVERVIEW_FIGURES_DIR
+from src.cmesrc.config import SWAN_DATA_DIR, ALL_MATCHING_HARPS_DATABASE_PICKLE, SCORED_HARPS_MATCHING_DIMMINGS_DATABASE_PICKLE, OVERVIEW_FIGURES_DIR, SCORED_HARPS_MATCHING_FLARES_DATABASE_PICKLE, MAIN_DATABASE_PICKLE
 from src.cmesrc.utils import get_closest_harps_timestamp
 from src.harps.harps import Harps
 from src.cmes.cmes import CME
+from src.flares.flares import Flare
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 from astropy.coordinates import Angle
@@ -23,15 +24,34 @@ transparent_white = (1, 1, 1, 0.4)
 
 #|%%--%%| <mfz9tKX7rF|WhTajwwSLc>
 
+main_data = pd.read_pickle(MAIN_DATABASE_PICKLE)
+
 harps_data = pd.read_pickle(ALL_MATCHING_HARPS_DATABASE_PICKLE)
 harps_data.set_index("CME_ID", drop=True, inplace=True)
 dimmings_data = pd.read_pickle(SCORED_HARPS_MATCHING_DIMMINGS_DATABASE_PICKLE)
 dimmings_data.set_index("CME_ID", drop=True, inplace=True)
 
-grouped_harps_data = harps_data.groupby("CME_ID")
-grouped_dimmings_data = dimmings_data.groupby("CME_ID")
+flares_data = pd.read_pickle(SCORED_HARPS_MATCHING_FLARES_DATABASE_PICKLE)
+flares_data.set_index("CME_ID", drop=True, inplace=True)
 
-#|%%--%%| <WhTajwwSLc|EF3HcZV3Xn>
+grouped_main_data = main_data.groupby("CME_ID")
+
+cme_match_mask = dict()
+for index, group in grouped_main_data:
+    cme_match_mask[index] =  np.any(group["DIMMING_MATCH"]) & np.any(group["FLARE_MATCH"])
+
+harps_data["plot"] = list(map(lambda x: False if x not in cme_match_mask.keys() else cme_match_mask[x], harps_data.index))
+
+grouped_harps_data = harps_data[harps_data["plot"]].groupby("CME_ID")
+grouped_dimmings_data = dimmings_data.groupby("CME_ID")
+grouped_flares_data = flares_data.groupby("CME_ID")
+
+
+
+#|%%--%%| <WhTajwwSLc|xM1yvXYPEM>
+
+
+#|%%--%%| <xM1yvXYPEM|EF3HcZV3Xn>
 
 def plot_cme(ax, sunpy_map, principal_angle, angular_width, halo = False, distance=0.95, npoints=100, linestyle="solid", color="green", linewidth=1, alpha=1):
     center = sunpy_map.world_to_pixel(sunpy_map.center)
@@ -55,7 +75,7 @@ def plot_cme(ax, sunpy_map, principal_angle, angular_width, halo = False, distan
     modified_y = []
 
     for angle in angles:
-        modified_x.append((rad * np.cos(angle)).value)
+        modified_x.append((rad * np.cos(angle)).value) 
         modified_y.append((rad * np.sin(angle)).value)
 
     if not halo:
@@ -90,7 +110,8 @@ def generate_blank_map():
 
 N = 500
 
-for cme_id in tqdm(np.random.choice(list(grouped_harps_data.groups.keys()), N, replace=False)):
+#for cme_id in tqdm(np.random.choice(list(grouped_harps_data.groups.keys()), N, replace=False)):
+for cme_id in tqdm(list(grouped_harps_data.groups.keys())):
 
     harps_rows = grouped_harps_data.get_group(cme_id)
 
@@ -98,6 +119,11 @@ for cme_id in tqdm(np.random.choice(list(grouped_harps_data.groups.keys()), N, r
         dimming_rows = grouped_dimmings_data.get_group(cme_id)
     else:
         dimming_rows = []
+
+    if cme_id in flares_data.index:
+        flare_rows = grouped_flares_data.get_group(cme_id)
+    else:
+        flare_rows = []
 
     cme_time = harps_rows["CME_DATE"].to_list()[0]
     cme_pa = harps_rows["CME_PA"].to_list()[0]
@@ -162,35 +188,27 @@ for cme_id in tqdm(np.random.choice(list(grouped_harps_data.groups.keys()), N, r
 
         ax.plot_coord(rotated_harps.get_centre_point().get_skycoord(), c="k", zorder=10, marker=".", markersize=1)
 
-    if len(dimming_rows) == 0:
-        continue
+    if len(dimming_rows) > 0:
+        for dimming_id, dimming_data_rows in dimming_rows.groupby("DIMMING_ID"):
+            dimming_data_rows = dimming_data_rows.sort_values(by="MATCH", ascending=False)
 
-    for dimming_id, dimming_data_rows in dimming_rows.groupby("DIMMING_ID"):
-        dimming_data_rows = dimming_data_rows.sort_values(by="MATCH", ascending=False)
+            dimming_data = dimming_data_rows.iloc[0]
+            matched_dimming = dimming_data["MATCH"] == 1
 
-        dimming_data = dimming_data_rows.iloc[0]
-        matched_dimming = dimming_data["MATCH"] == 1
+            if matched_dimming:
+                color = "#28C0D7"
+            else:
+                color = "#D73F28"
 
-        if matched_dimming:
-            color = "#28C0D7"
-        else:
-            color = "#D73F28"
+            dimming = Dimming(
+                    date = dimming_data["DIMMING_TIME"],
+                    lon = dimming_data["DIMMING_LON"],
+                    lat = dimming_data["DIMMING_LAT"]
+                    )
 
-        dimming = Dimming(
-                date = dimming_data["DIMMING_TIME"],
-                lon = dimming_data["DIMMING_LON"],
-                lat = dimming_data["DIMMING_LAT"]
-                )
+            dimming_skycoord = dimming.point.rotate_coords(cme_time).get_skycoord()
 
-        dimming_skycoord = dimming.point.rotate_coords(cme_time).get_skycoord()
-
-        ax.plot_coord(dimming_skycoord, c=color, zorder=10, marker="x")
-
-            
-        if matched_dimming:
-            harps_centre = plotted_harps_dict[dimming_data["HARPNUM"]].get_centre_point().get_skycoord()
-            line = SkyCoord([dimming_skycoord, harps_centre])
-            ax.plot_coord(line, c="k", linewidth=1, zorder=5)
+            ax.plot_coord(dimming_skycoord, c=color, zorder=10, marker="X", markersize=10, markeredgecolor="k")
 
             ax.annotate(f"D{dimming_id}", 
                         (0, 0),
@@ -204,7 +222,53 @@ for cme_id in tqdm(np.random.choice(list(grouped_harps_data.groups.keys()), N, r
                         zorder=10
                         )
 
-    plt.savefig(OVERVIEW_FIGURES_DIR + str(cme_id) + ".png", dpi=150, facecolor="#555555")
+                
+            if matched_dimming:
+                harps_centre = plotted_harps_dict[dimming_data["HARPNUM"]].get_centre_point().get_skycoord()
+                line = SkyCoord([dimming_skycoord, harps_centre])
+                ax.plot_coord(line, c="k", linewidth=1, zorder=5)
+
+
+    if len(flare_rows) > 1:
+        for flare_id, flare_data_rows in flare_rows.groupby("FLARE_ID"):
+            flare_data_rows = flare_data_rows.sort_values(by="MATCH", ascending=False)
+
+            flare_data = flare_data_rows.iloc[0]
+            matched_flare = flare_data["MATCH"] == 1
+
+            if matched_flare:
+                color = "#28C0D7"
+            else:
+                color = "#D73F28"
+
+            flare = Flare(
+                    date = flare_data["FLARE_TIME"],
+                    lon = flare_data["FLARE_LON"],
+                    lat = flare_data["FLARE_LAT"]
+                    )
+
+            flare_skycoord = flare.point.rotate_coords(cme_time).get_skycoord()
+
+            ax.plot_coord(flare_skycoord, c=color, zorder=10, marker="*", markersize=10, markeredgecolor="k")
+
+            ax.annotate(f"F{flare_id}", 
+                        (0, 0),
+                        xytext=(flare.point.LON, flare.point.LAT + 5),
+                        xycoords=ax.get_transform('heliographic_stonyhurst'),
+                        backgroundcolor=transparent_white,
+                        color='k',
+                        horizontalalignment='center', 
+                        verticalalignment='top',
+                        fontsize=6,
+                        zorder=10
+                        )
+                
+            if matched_flare:
+                harps_centre = plotted_harps_dict[flare_data["HARPNUM"]].get_centre_point().get_skycoord()
+                line = SkyCoord([flare_skycoord, harps_centre])
+                ax.plot_coord(line, c="k", linewidth=1, zorder=5)
+
+    plt.savefig(OVERVIEW_FIGURES_DIR + str(cme_id) + ".png", dpi=100, facecolor="#555555")
     plt.clf()
     plt.cla()
     plt.close()
