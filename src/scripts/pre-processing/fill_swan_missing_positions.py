8831b0bb63c1775a86a54cab9624897b1b3aa0f9
Julio Hernandez Camero <julhcam@pm.me>
2023-04-27 13:14:33 +0100

    Checkpoint

--- src/scripts/pre-processing/fill_swan_missing_positions.py
+++ src/scripts/pre-processing/fill_swan_missing_positions.py
@@ -10,6 +10,10 @@ clear_screen()
 
 SWAN = cache_swan_data()
 
+# SWAN = {
+#     "test": pd.read_csv("/home/julio/cmesrc/data/raw/mvts/SWAN/partition4/4588.csv", sep="\t")
+# }
+
 clear_screen()
 
 print("== FILLING MISSING VALUES ==")
@@ -40,8 +44,9 @@ def get_nan_intervals(nan_bbox_mask):
         
 
 for harpnum, swan_harp in tqdm(SWAN.items()):
-    swan_harp.to_csv(join(UPDATED_SWAN, "original.csv"), sep="\t", index=False)
-    swan_harp["IRBB"] = False
+    new_swan_harp = swan_harp.copy()
+
+    new_swan_harp["IRBB"] = False
 
     nan_bbox_mask = swan_harp[["LON_MIN", "LON_MAX", "LAT_MIN", "LAT_MAX"]].isna().any(axis=1)
 
@@ -52,42 +57,47 @@ for harpnum, swan_harp in tqdm(SWAN.items()):
 
         middle = (start + end) // 2
 
-        if end == len(swan_harp):
-            end = len(swan_harp) - 1
-
-        if start == 0:
-            ref_index = end + 1
+        if start != 0:
+            first_index = start - 1
         else:
-            ref_index = start - 1
+            first_index = end
 
-        ref_row = swan_harp.iloc[ref_index]
+        if end != len(new_swan_harp):
+            last_index = end
+        else:
+            last_index = start - 1
 
-        harp_data = ref_row[["Timestamp", "LON_MIN", "LAT_MIN", "LON_MAX", "LAT_MAX"]].to_list()
+        first_ref_row = swan_harp.iloc[first_index]
+        last_ref_row = swan_harp.iloc[last_index]
 
-        harps = Harps(*harp_data)
+        first_harp_data = first_ref_row[["Timestamp", "LON_MIN", "LAT_MIN", "LON_MAX", "LAT_MAX"]].to_list()
+        last_harp_data = last_ref_row[["Timestamp", "LON_MIN", "LAT_MIN", "LON_MAX", "LAT_MAX"]].to_list()
+
+        try:
+            first_harps = Harps(*first_harp_data)
+        except (ValueError, TypeError):
+            print("FIRST HARP DATA")
+            print(harpnum)
+            print(first_harp_data)
+            raise
+
+        try:
+            last_harps = Harps(*last_harp_data)
+        except (ValueError, TypeError):
+            print("LAST HARP DATA")
+            print(harpnum)
+            print(last_harp_data)
+            print(last_ref_row)
+            raise
 
         for i in range(start, end):
-            if i <= middle and start != 0:
-                ref_index = start - 1
-            elif start == 0:
-                ref_index = end
-            
-            elif i > middle and end != len(swan_harp):
-                ref_index = end
-
-            elif end == len(swan_harp):
-                ref_index = start - 1
-            
+            if i <= middle:
+                harps = first_harps
             else:
-                raise ValueError("Something went wrong")
-
-            ref_row = swan_harp.iloc[ref_index]
-
-            harp_data = ref_row[["Timestamp", "LON_MIN", "LAT_MIN", "LON_MAX", "LAT_MAX"]].to_list()
-
-            harps = Harps(*harp_data)
+                harps = last_harps
 
             incomplete_row = swan_harp.iloc[i]
+            incomplete_index = incomplete_row.name
 
             new_timestamp = incomplete_row["Timestamp"]
 
@@ -95,13 +105,13 @@ for harpnum, swan_harp in tqdm(SWAN.items()):
 
             new_bbox = rotated_harps.get_raw_bbox()
 
-            swan_harp.at[i, "LON_MIN"] = new_bbox[0][0]
-            swan_harp.at[i, "LAT_MIN"] = new_bbox[0][1]
-            swan_harp.at[i, "LON_MAX"] = new_bbox[1][0]
-            swan_harp.at[i, "LAT_MAX"] = new_bbox[1][1]
+            new_swan_harp.at[incomplete_index, "LON_MIN"] = new_bbox[0][0]
+            new_swan_harp.at[incomplete_index, "LAT_MIN"] = new_bbox[0][1]
+            new_swan_harp.at[incomplete_index, "LON_MAX"] = new_bbox[1][0]
+            new_swan_harp.at[incomplete_index, "LAT_MAX"] = new_bbox[1][1]
 
-            swan_harp.at[i, "IRBB"] = True
+            new_swan_harp.at[incomplete_index, "IRBB"] = True
 
     filename = f"{harpnum}.csv" 
 
-    swan_harp.to_csv(join(UPDATED_SWAN, filename), sep="\t", index=False)
+    new_swan_harp.to_csv(join(UPDATED_SWAN, filename), sep="\t", index=False)
